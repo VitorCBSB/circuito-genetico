729be2e9f801645a09bfb345e91603641e2a959a
Gabriel Arao <gabrielarao2013@gmail.com>
2018-04-09 17:20:22 -0300

    More PIOs

--- testeio/testeio_bb.v
+++ testeio/testeio_bb.v
@@ -117,7 +117,184 @@ module testeio (
 	valid_output_1_export,
 	valid_output_2_export,
 	valid_output_3_export,
-	valid_output_4_export);	
+	valid_output_4_export,
+	valid_output_5_export,
+	valid_output_6_export,
+	valid_output_7_export,
+	valid_output_8_export,
+	valid_output_9_export,
+	valid_output_10_export,
+	valid_output_11_export,
+	valid_output_12_export,
+	valid_output_13_export,
+	valid_output_14_export,
+	valid_output_15_export,
+	valid_output_16_export,
+	valid_output_17_export,
+	valid_output_18_export,
+	valid_output_19_export,
+	valid_output_20_export,
+	valid_output_21_export,
+	valid_output_22_export,
+	valid_output_23_export,
+	valid_output_24_export,
+	valid_output_25_export,
+	input_sequence_62_export,
+	valid_output_26_export,
+	valid_output_27_export,
+	valid_output_28_export,
+	valid_output_29_export,
+	valid_output_30_export,
+	valid_output_31_export,
+	valid_output_32_export,
+	valid_output_33_export,
+	valid_output_34_export,
+	valid_output_35_export,
+	valid_output_36_export,
+	valid_output_37_export,
+	valid_output_38_export,
+	valid_output_39_export,
+	valid_output_40_export,
+	valid_output_41_export,
+	valid_output_42_export,
+	valid_output_43_export,
+	valid_output_44_export,
+	valid_output_45_export,
+	valid_output_46_export,
+	valid_output_47_export,
+	valid_output_48_export,
+	valid_output_49_export,
+	valid_output_50_export,
+	valid_output_51_export,
+	valid_output_52_export,
+	valid_output_53_export,
+	valid_output_54_export,
+	valid_output_55_export,
+	valid_output_56_export,
+	valid_output_57_export,
+	valid_output_58_export,
+	valid_output_59_export,
+	valid_output_60_export,
+	valid_output_61_export,
+	valid_output_62_export,
+	valid_output_63_export,
+	input_sequence_5_export,
+	input_sequence_6_export,
+	input_sequence_7_export,
+	input_sequence_8_export,
+	input_sequence_9_export,
+	input_sequence_10_export,
+	input_sequence_11_export,
+	input_sequence_12_export,
+	input_sequence_13_export,
+	input_sequence_14_export,
+	input_sequence_15_export,
+	input_sequence_16_export,
+	input_sequence_17_export,
+	input_sequence_18_export,
+	input_sequence_19_export,
+	input_sequence_20_export,
+	input_sequence_21_export,
+	input_sequence_22_export,
+	input_sequence_23_export,
+	input_sequence_24_export,
+	input_sequence_25_export,
+	input_sequence_26_export,
+	input_sequence_27_export,
+	input_sequence_28_export,
+	input_sequence_29_export,
+	input_sequence_30_export,
+	input_sequence_31_export,
+	input_sequence_32_export,
+	input_sequence_33_export,
+	input_sequence_34_export,
+	input_sequence_35_export,
+	input_sequence_36_export,
+	input_sequence_37_export,
+	input_sequence_38_export,
+	input_sequence_39_export,
+	input_sequence_40_export,
+	input_sequence_41_export,
+	input_sequence_42_export,
+	input_sequence_43_export,
+	input_sequence_44_export,
+	input_sequence_45_export,
+	input_sequence_46_export,
+	input_sequence_47_export,
+	input_sequence_48_export,
+	input_sequence_49_export,
+	input_sequence_50_export,
+	input_sequence_51_export,
+	input_sequence_52_export,
+	input_sequence_53_export,
+	input_sequence_54_export,
+	input_sequence_55_export,
+	input_sequence_56_export,
+	input_sequence_57_export,
+	input_sequence_58_export,
+	input_sequence_59_export,
+	input_sequence_60_export,
+	input_sequence_61_export,
+	input_sequence_63_export,
+	expected_output_5_export,
+	expected_output_6_export,
+	expected_output_7_export,
+	expected_output_8_export,
+	expected_output_9_export,
+	expected_output_10_export,
+	expected_output_11_export,
+	expected_output_12_export,
+	expected_output_13_export,
+	expected_output_14_export,
+	expected_output_15_export,
+	expected_output_16_export,
+	expected_output_17_export,
+	expected_output_18_export,
+	expected_output_19_export,
+	expected_output_20_export,
+	expected_output_21_export,
+	expected_output_22_export,
+	expected_output_23_export,
+	expected_output_24_export,
+	expected_output_25_export,
+	expected_output_26_export,
+	expected_output_27_export,
+	expected_output_28_export,
+	expected_output_29_export,
+	expected_output_30_export,
+	expected_output_31_export,
+	expected_output_32_export,
+	expected_output_33_export,
+	expected_output_34_export,
+	expected_output_35_export,
+	expected_output_36_export,
+	expected_output_37_export,
+	expected_output_38_export,
+	expected_output_39_export,
+	expected_output_40_export,
+	expected_output_41_export,
+	expected_output_42_export,
+	expected_output_43_export,
+	expected_output_44_export,
+	expected_output_45_export,
+	expected_output_46_export,
+	expected_output_47_export,
+	expected_output_48_export,
+	expected_output_49_export,
+	expected_output_50_export,
+	expected_output_51_export,
+	expected_output_52_export,
+	expected_output_53_export,
+	expected_output_54_export,
+	expected_output_55_export,
+	expected_output_56_export,
+	expected_output_57_export,
+	expected_output_58_export,
+	expected_output_59_export,
+	expected_output_60_export,
+	expected_output_61_export,
+	expected_output_62_export,
+	expected_output_63_export);	
 
 	output	[31:0]	chrom_seg_0_export;
 	output	[31:0]	chrom_seg_1_export;
@@ -237,4 +414,181 @@ module testeio (
 	output	[31:0]	valid_output_2_export;
 	output	[31:0]	valid_output_3_export;
 	output	[31:0]	valid_output_4_export;
+	output	[31:0]	valid_output_5_export;
+	output	[31:0]	valid_output_6_export;
+	output	[31:0]	valid_output_7_export;
+	output	[31:0]	valid_output_8_export;
+	output	[31:0]	valid_output_9_export;
+	output	[31:0]	valid_output_10_export;
+	output	[31:0]	valid_output_11_export;
+	output	[31:0]	valid_output_12_export;
+	output	[31:0]	valid_output_13_export;
+	output	[31:0]	valid_output_14_export;
+	output	[31:0]	valid_output_15_export;
+	output	[31:0]	valid_output_16_export;
+	output	[31:0]	valid_output_17_export;
+	output	[31:0]	valid_output_18_export;
+	output	[31:0]	valid_output_19_export;
+	output	[31:0]	valid_output_20_export;
+	output	[31:0]	valid_output_21_export;
+	output	[31:0]	valid_output_22_export;
+	output	[31:0]	valid_output_23_export;
+	output	[31:0]	valid_output_24_export;
+	output	[31:0]	valid_output_25_export;
+	output	[31:0]	input_sequence_62_export;
+	output	[31:0]	valid_output_26_export;
+	output	[31:0]	valid_output_27_export;
+	output	[31:0]	valid_output_28_export;
+	output	[31:0]	valid_output_29_export;
+	output	[31:0]	valid_output_30_export;
+	output	[31:0]	valid_output_31_export;
+	output	[31:0]	valid_output_32_export;
+	output	[31:0]	valid_output_33_export;
+	output	[31:0]	valid_output_34_export;
+	output	[31:0]	valid_output_35_export;
+	output	[31:0]	valid_output_36_export;
+	output	[31:0]	valid_output_37_export;
+	output	[31:0]	valid_output_38_export;
+	output	[31:0]	valid_output_39_export;
+	output	[31:0]	valid_output_40_export;
+	output	[31:0]	valid_output_41_export;
+	output	[31:0]	valid_output_42_export;
+	output	[31:0]	valid_output_43_export;
+	output	[31:0]	valid_output_44_export;
+	output	[31:0]	valid_output_45_export;
+	output	[31:0]	valid_output_46_export;
+	output	[31:0]	valid_output_47_export;
+	output	[31:0]	valid_output_48_export;
+	output	[31:0]	valid_output_49_export;
+	output	[31:0]	valid_output_50_export;
+	output	[31:0]	valid_output_51_export;
+	output	[31:0]	valid_output_52_export;
+	output	[31:0]	valid_output_53_export;
+	output	[31:0]	valid_output_54_export;
+	output	[31:0]	valid_output_55_export;
+	output	[31:0]	valid_output_56_export;
+	output	[31:0]	valid_output_57_export;
+	output	[31:0]	valid_output_58_export;
+	output	[31:0]	valid_output_59_export;
+	output	[31:0]	valid_output_60_export;
+	output	[31:0]	valid_output_61_export;
+	output	[31:0]	valid_output_62_export;
+	output	[31:0]	valid_output_63_export;
+	output	[31:0]	input_sequence_5_export;
+	output	[31:0]	input_sequence_6_export;
+	output	[31:0]	input_sequence_7_export;
+	output	[31:0]	input_sequence_8_export;
+	output	[31:0]	input_sequence_9_export;
+	output	[31:0]	input_sequence_10_export;
+	output	[31:0]	input_sequence_11_export;
+	output	[31:0]	input_sequence_12_export;
+	output	[31:0]	input_sequence_13_export;
+	output	[31:0]	input_sequence_14_export;
+	output	[31:0]	input_sequence_15_export;
+	output	[31:0]	input_sequence_16_export;
+	output	[31:0]	input_sequence_17_export;
+	output	[31:0]	input_sequence_18_export;
+	output	[31:0]	input_sequence_19_export;
+	output	[31:0]	input_sequence_20_export;
+	output	[31:0]	input_sequence_21_export;
+	output	[31:0]	input_sequence_22_export;
+	output	[31:0]	input_sequence_23_export;
+	output	[31:0]	input_sequence_24_export;
+	output	[31:0]	input_sequence_25_export;
+	output	[31:0]	input_sequence_26_export;
+	output	[31:0]	input_sequence_27_export;
+	output	[31:0]	input_sequence_28_export;
+	output	[31:0]	input_sequence_29_export;
+	output	[31:0]	input_sequence_30_export;
+	output	[31:0]	input_sequence_31_export;
+	output	[31:0]	input_sequence_32_export;
+	output	[31:0]	input_sequence_33_export;
+	output	[31:0]	input_sequence_34_export;
+	output	[31:0]	input_sequence_35_export;
+	output	[31:0]	input_sequence_36_export;
+	output	[31:0]	input_sequence_37_export;
+	output	[31:0]	input_sequence_38_export;
+	output	[31:0]	input_sequence_39_export;
+	output	[31:0]	input_sequence_40_export;
+	output	[31:0]	input_sequence_41_export;
+	output	[31:0]	input_sequence_42_export;
+	output	[31:0]	input_sequence_43_export;
+	output	[31:0]	input_sequence_44_export;
+	output	[31:0]	input_sequence_45_export;
+	output	[31:0]	input_sequence_46_export;
+	output	[31:0]	input_sequence_47_export;
+	output	[31:0]	input_sequence_48_export;
+	output	[31:0]	input_sequence_49_export;
+	output	[31:0]	input_sequence_50_export;
+	output	[31:0]	input_sequence_51_export;
+	output	[31:0]	input_sequence_52_export;
+	output	[31:0]	input_sequence_53_export;
+	output	[31:0]	input_sequence_54_export;
+	output	[31:0]	input_sequence_55_export;
+	output	[31:0]	input_sequence_56_export;
+	output	[31:0]	input_sequence_57_export;
+	output	[31:0]	input_sequence_58_export;
+	output	[31:0]	input_sequence_59_export;
+	output	[31:0]	input_sequence_60_export;
+	output	[31:0]	input_sequence_61_export;
+	output	[31:0]	input_sequence_63_export;
+	output	[31:0]	expected_output_5_export;
+	output	[31:0]	expected_output_6_export;
+	output	[31:0]	expected_output_7_export;
+	output	[31:0]	expected_output_8_export;
+	output	[31:0]	expected_output_9_export;
+	output	[31:0]	expected_output_10_export;
+	output	[31:0]	expected_output_11_export;
+	output	[31:0]	expected_output_12_export;
+	output	[31:0]	expected_output_13_export;
+	output	[31:0]	expected_output_14_export;
+	output	[31:0]	expected_output_15_export;
+	output	[31:0]	expected_output_16_export;
+	output	[31:0]	expected_output_17_export;
+	output	[31:0]	expected_output_18_export;
+	output	[31:0]	expected_output_19_export;
+	output	[31:0]	expected_output_20_export;
+	output	[31:0]	expected_output_21_export;
+	output	[31:0]	expected_output_22_export;
+	output	[31:0]	expected_output_23_export;
+	output	[31:0]	expected_output_24_export;
+	output	[31:0]	expected_output_25_export;
+	output	[31:0]	expected_output_26_export;
+	output	[31:0]	expected_output_27_export;
+	output	[31:0]	expected_output_28_export;
+	output	[31:0]	expected_output_29_export;
+	output	[31:0]	expected_output_30_export;
+	output	[31:0]	expected_output_31_export;
+	output	[31:0]	expected_output_32_export;
+	output	[31:0]	expected_output_33_export;
+	output	[31:0]	expected_output_34_export;
+	output	[31:0]	expected_output_35_export;
+	output	[31:0]	expected_output_36_export;
+	output	[31:0]	expected_output_37_export;
+	output	[31:0]	expected_output_38_export;
+	output	[31:0]	expected_output_39_export;
+	output	[31:0]	expected_output_40_export;
+	output	[31:0]	expected_output_41_export;
+	output	[31:0]	expected_output_42_export;
+	output	[31:0]	expected_output_43_export;
+	output	[31:0]	expected_output_44_export;
+	output	[31:0]	expected_output_45_export;
+	output	[31:0]	expected_output_46_export;
+	output	[31:0]	expected_output_47_export;
+	output	[31:0]	expected_output_48_export;
+	output	[31:0]	expected_output_49_export;
+	output	[31:0]	expected_output_50_export;
+	output	[31:0]	expected_output_51_export;
+	output	[31:0]	expected_output_52_export;
+	output	[31:0]	expected_output_53_export;
+	output	[31:0]	expected_output_54_export;
+	output	[31:0]	expected_output_55_export;
+	output	[31:0]	expected_output_56_export;
+	output	[31:0]	expected_output_57_export;
+	output	[31:0]	expected_output_58_export;
+	output	[31:0]	expected_output_59_export;
+	output	[31:0]	expected_output_60_export;
+	output	[31:0]	expected_output_61_export;
+	output	[31:0]	expected_output_62_export;
+	output	[31:0]	expected_output_63_export;
 endmodule
